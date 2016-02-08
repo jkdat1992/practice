2cbdb6b1ef01340cea6ae0844c1dadc12b6e9fa1
jkdat1992 <jkdat1992@gmail.com>
2016-02-08 15:10:11 +0700

    change config run database to mysql-server

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_test',
+        'USER': 'root',
+        'PASSWORD': 'Tiendat1992a@',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 

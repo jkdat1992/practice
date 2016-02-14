90dbe3143b7d4a3bea40ccdce6c7f0de5ba433b3
jkdat1992 <jkdat1992@gmail.com>
2016-02-14 23:41:15 +0700

    update and fix CSS static file

--- practice/CustomerAuth/forms.py
+++ practice/CustomerAuth/forms.py
@@ -1,35 +1,25 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.forms import ModelForm
-from CustomerAuth.models import Cusomter
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import  Submit
 
-class RegistrationForm(ModelForm):
+class RegistrationForm(forms.Form):
         username    = forms.CharField(label=(u'User Name'))
         email       = forms.EmailField(label=(u'Email Address'))
         password    = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
         password1   = forms.CharField(label=(u'Verify Password'), widget=forms.PasswordInput(render_value=False))
 
-        class Meta:
-                model = Cusomter
-                exclude =  ('user',)
-        def clean_username(self):
-            username = self.cleaned_data['username']
-            print username
-            try:
-                    User.objects.get(username=username)
-            except User.DoesNotExist:
-                return username
-            raise forms.ValidationError("that username is already taken, please select another")
+        def __init__(self , *args , **kwargs):
+            super(RegistrationForm,self).__init__(*args,**kwargs)
+            self.helper= FormHelper()
+            self.helper.form_class='col-lg-6 col-sm-4'
+            self.helper.add_input(Submit('submit', 'Submit'))
+            self.helper.form_class = 'form-horizontal'
+            self.helper.label_class = 'col-lg-3'
+            self.helper.field_class = 'col-lg-8'
 
-        def clean_password(self):
-            password1 = self.cleaned_data['password1']
-            password  = self.cleaned_data['password']
-            print password
 
-            print password1
-            if password != password1:
-                raise forms.ValidationError("The passwords did not match , Please try again")
-            return password
+
+
 
 
 
--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_forms'
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -128,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join( BASE_DIR, 'statics')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
 #MEDIA_ROOT = '/Users/jmitch/Desktop/ecommerce/static/media/'
